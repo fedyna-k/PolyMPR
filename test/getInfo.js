b3fbe6b6c87e05e048697619d3326b941d46232f
Kevin FEDYNA <fedyna.kevin@gmail.com>
2024-12-02 14:21:07 +0100

    Added first mock design and CAS tests

---
+++ test/getInfo.js
@@ -0,0 +1,15 @@
+const token = process.env.CAS_TOKEN;
+
+async function main() {
+  const response = await fetch("https://ident.univ-amu.fr/login?service=http://localhost", {
+    "Cookie": `TGC=${token}`
+  });
+  console.log(response.headers);
+
+
+  const responseText = await response.text();
+
+  console.log(responseText);
+}
+
+main();
