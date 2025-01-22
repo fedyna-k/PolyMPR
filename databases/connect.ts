6456529b7f0e9973bc58fd38a102ea133018ff0e
Kevin FEDYNA <fedyna.kevin@gmail.com>
2025-01-22 14:48:00 +0100

    Added documentation for connect file

--- databases/connect.ts
+++ databases/connect.ts
@@ -4,6 +4,11 @@ interface DatabaseConnection extends Disposable {
   database: Database;
 }
 
+/**
+ * Connect to the given database and attach `students.db`.
+ * @param database The database name.
+ * @returns The database connection as disposable.
+ */
 export default function connect(database: string): DatabaseConnection {
   const connection = new Database(`databases/data/${database}.db`, {
     create: false,
